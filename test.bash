b6cf467382baa3f66f9d8bd88477537283abfa5e
raimu <koba54051@gmail.com>
2024-10-31 13:03:43 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 kohei kobayashi
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -8,7 +8,18 @@ ng (){
 }
 
 res=0
- out=$(seq 5 | ./plus)
+
+### 間違ってる入力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+### 合ってる入力 ###
+out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
